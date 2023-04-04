4fbb82070a8b73294b9d10387d51675ba07efbcb
Jeonghyun Ryu <jhforstudy@gmail.com>
2023-04-04 23:27:09 +0900

    중간 2

--- system_program/hpfp/hpfp.c
+++ system_program/hpfp/hpfp.c
@@ -3,24 +3,22 @@
 #include <stdlib.h>
 
 hpfp int_converter(int input){
-    int deci_to_binary[11];     // 변환한 2진법 저장
-    int result_hpfp[16];        // 결과
+    short int deci_to_binary[11];     // 변환한 2진법 저장
+    short int result_hpfp[16];        // 결과
     int i = 0;
     int cnt = -1;               // 2진법의 마지막 index
     int S, M, E;
     int bias, exp;
-    int tmp;
+    hpfp result = 0;
+	int tmp = 1;
 
-    printf("%d\n", input);
-
-    // array 초기화
-    for (i=0; i<11; i++)
-        deci_to_binary[i] = 0;
+	// array 초기화
+	for (i=0; i<11; i++)
+		deci_to_binary[i] = 0;
 
 	// input을 2진법으로 변환
     for (i=0; input>=1; i++) {
         deci_to_binary[i] = input % 2;
-        printf("%d ", deci_to_binary[i]);
         input /= 2;
         cnt++;
     }
@@ -42,7 +40,7 @@ hpfp int_converter(int input){
     result_hpfp[0] = S;
     // exp  : [1:5]
     // exp를 2진법으로 변환하며, 역순으로 대입
-    for (i=5; i>=1; i++) {
+    for (i=5; i>=1; i--) {
         result_hpfp[i] = exp % 2;
         exp /= 2;
     }
@@ -50,14 +48,26 @@ hpfp int_converter(int input){
     // M에서 cnt-1 인덱스 제외하고, 역순으로 대입
     for (i=0; i<cnt; i++) {
         // [6] ~ [6+cnt-1]
+		// printf("%d <- %d\n", i+6, deci_to_binary[cnt-1-i]);
         result_hpfp[i+6] = deci_to_binary[cnt-1-i];
     }
     for (i=cnt; i<10; i++) {
+		// printf("%d <- %d\n", i+6, 0);
         result_hpfp[i+6] = 0;
     }
 
-    for (i=0; i<16; i++)
-        printf("%d", result_hpfp[i]);
+	for (i=0; i<16; i++)
+		printf("%d", result_hpfp[i]);
+	printf("\n");
+
+	// 배열 -> unsigned short
+	for (i=15; i>=0; i--) {
+		result += result_hpfp[i] * tmp;
+		tmp *= 2;
+	}
+	printf("%d\n", result);
+
+	return result;
 }
 
 int hpfp_to_int_converter(hpfp input){
@@ -86,5 +96,14 @@ float float_flipper(float input){
 }
 
 char* hpfp_to_bits_converter(hpfp result){
+	printf("%d", result);
+	char result_str[16];
+	char* ptr = result_str;
 
+	for (int i=15; i>=0; i++) {
+		result_str[i] = result % 2;
+		result /= 2;
+	}
+
+	return ptr;
 }
