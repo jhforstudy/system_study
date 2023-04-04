f5b6bc69209cc785534e433b9de7161beb3d7dbc
Jeonghyun Ryu <jhforstudy@gmail.com>
2023-04-04 21:44:55 +0900

    중간과정

--- system_program/hpfp/hpfp.c
+++ system_program/hpfp/hpfp.c
@@ -1,8 +1,63 @@
 #include "hpfp.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 hpfp int_converter(int input){
-	
+    int deci_to_binary[11];     // 변환한 2진법 저장
+    int result_hpfp[16];        // 결과
+    int i = 0;
+    int cnt = -1;               // 2진법의 마지막 index
+    int S, M, E;
+    int bias, exp;
+    int tmp;
+
+    printf("%d\n", input);
+
+    // array 초기화
+    for (i=0; i<11; i++)
+        deci_to_binary[i] = 0;
+
+	// input을 2진법으로 변환
+    for (i=0; input>=1; i++) {
+        deci_to_binary[i] = input % 2;
+        printf("%d ", deci_to_binary[i]);
+        input /= 2;
+        cnt++;
+    }
+    // (-1)^S * M * 2^E 형태로 나타내기
+    // S = 0 (양수 or 0) ; 1 (음수)
+    if (input < 0) S = 1;
+    else S = 0;
+    // M = deci_to_binary[0:cnt-1]
+    // E = cnt;
+    E = cnt;
+
+    // bias = 2^(5-1)-1 = 15
+    // exp = bias + E
+    bias = 15;
+    exp = bias + E;
+
+    // 결과 hpfp에 채우기
+    // S    : [0]
+    result_hpfp[0] = S;
+    // exp  : [1:5]
+    // exp를 2진법으로 변환하며, 역순으로 대입
+    for (i=5; i>=1; i++) {
+        result_hpfp[i] = exp % 2;
+        exp /= 2;
+    }
+    // frac : [6:15]
+    // M에서 cnt-1 인덱스 제외하고, 역순으로 대입
+    for (i=0; i<cnt; i++) {
+        // [6] ~ [6+cnt-1]
+        result_hpfp[i+6] = deci_to_binary[cnt-1-i];
+    }
+    for (i=cnt; i<10; i++) {
+        result_hpfp[i+6] = 0;
+    }
+
+    for (i=0; i<16; i++)
+        printf("%d", result_hpfp[i]);
 }
 
 int hpfp_to_int_converter(hpfp input){
